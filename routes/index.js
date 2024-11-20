4f48d0538fc4a371b1a726a748116396fa47d1ee
Peter Brown <phbrown@acm.org>
2024-11-19 19:07:08 -0500

    Added query parameterized with form data.

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  clearReqAppLocals(req);
   topLevel(req, res, next);
 });
 
 router.post('/', function(req, res, next) {
+  clearReqAppLocals(req);
+  req.app.locals.formdata = req.body;
   topLevel(req, res, next, req.body);
 });
 
-function topLevel(req, res, next, formdata) {
-  clearReqAppLocals(req);
-  req.app.locals.formdata = formdata;
+/*
+ * Initial function.  Performs a query on the database, attaches the result
+ * to req.app.locals, and passes control to the next query.
+ */
+function topLevel(req, res, next) {
+  if (req.app.locals.formdata) {
+    console.log('Form data: ' + JSON.stringify(req.app.locals.formdata));
+  }
+  else { console.log('No form data'); }
+  
   if (req.app.locals.db) {
     console.log('Got the database');
     req.app.locals.query = "select * from Faculty;";
@@ -21,7 +31,7 @@ function topLevel(req, res, next, formdata) {
         throw err;
       }
       req.app.locals.rows = rows;
-      showIndex(req, res, next); // Has to happen inside the handler
+      parameterizedQuery(req, res, next); // Has to happen inside the handler
     });
   }
   else {
@@ -33,15 +43,71 @@ function topLevel(req, res, next, formdata) {
 function clearReqAppLocals(req) {
   req.app.locals.query = '';
   req.app.locals.rows = [];
-  req.app.locals.schools = [];
+  req.app.locals.FacSSN = '';
+  req.app.locals.paramQuery = '';
   req.app.locals.courses = [];
   req.app.locals.formdata = {};
 }
 
+/*
+ * Performs a query on the database, using form data to parameterize the query.
+ */
+function parameterizedQuery(req, res, next) {
+  if (req.app.locals.formdata && req.app.locals.formdata.FacSSN) {
+    console.log('FacSSN: ' + req.app.locals.formdata.FacSSN);
+    // Normalize the SSN to have dashes
+    req.app.locals.FacSSN = SSN_with_dashes(req.app.locals.formdata.FacSSN);
+    // Set the version for the query *not* to have dashes, since that's what's in the database.
+    let querySSN = req.app.locals.formdata.FacSSN.replaceAll('-', '');
+
+    // The query contains "?" where the parameters are to be inserted.  The
+    // second argument to db.all() is an array of parameter values to fill in
+    // where the query has a "?".  See 
+    // https://github.com/TryGhost/node-sqlite3/wiki/API#runsql--param---callback
+    // for more  details.
+    // This query could also be constructed as a complete string (with no "?" and
+    // querySSN already substituted in) in Javascript.  In that case, the second
+    // argument to db.all() would just be an empty array.
+    let paramQuery = "select * from Offering where FacSSN = ?"
+    req.app.locals.db.all(paramQuery, [querySSN], 
+                          (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.paramQuery = paramQuery;
+      req.app.locals.courses = rows; // Different name from the last time, to keep
+                                     // responses to different queries separate
+      showIndex(req, res, next); 
+    });
+  }
+  else {
+    showIndex(req, res, next);
+  }
+
+}
+
+/*
+ * Takes a string and ensures that the fourth and seventh characters are hyphens.
+ * If they already are, returns the string unchanged.
+ */
+function SSN_with_dashes(ssn) {
+  if (ssn.at(3) != '-') {
+    ssn = ssn.slice(0, 3) + '-' + ssn.slice(3);
+  }
+  if (ssn.at(6) != '-') {
+    ssn = ssn.slice(0, 6) + '-' + ssn.slice(6);
+  }
+  return ssn;
+}
+
+
 function showIndex(req, res, next) {
   res.render('index', { title: 'Another brick in the SQL',
+                        query: req.app.locals.query,
                         rows: req.app.locals.rows,
-                        FacSSN: req.app.locals.formdata.FacSSN,
+                        FacSSN: req.app.locals.FacSSN,
+                        paramQuery: req.app.locals.paramQuery,
+                        courses: req.app.locals.courses,
                         formdata: req.app.locals.formdata
   });
 }
